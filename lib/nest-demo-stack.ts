df9b03b56862d95d136b1578ffde715bedb74558
Yatin kumar <yatinkumar@Yatins-MacBook-Air.local>
2023-06-02 12:56:58 +0530

    init

---
+++ lib/nest-demo-stack.ts
@@ -0,0 +1,45 @@
+import * as cdk from "@aws-cdk/core";
+import { Vpc } from "@aws-cdk/aws-ec2";
+import * as ecs from "@aws-cdk/aws-ecs";
+import * as ecs_patterns from "@aws-cdk/aws-ecs-patterns";
+
+export class DemoStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+    
+    // VPC
+    const vpc = new Vpc(this, "demoVPC", {
+      maxAzs: 2,
+      natGateways: 1,
+    });
+
+    // Fargate cluster
+    const cluster = new ecs.Cluster(this, "demoCluster", {
+      vpc: vpc as any,
+    });
+
+    // Fargate service
+    const backendService = new ecs_patterns.ApplicationLoadBalancedFargateService(this, "demoService", {
+      cluster: cluster,
+      memoryLimitMiB: 1024,
+      cpu: 512,
+      desiredCount: 2,
+      taskImageOptions: {
+        image: ecs.ContainerImage.fromAsset("../nestjs-template/backend/"),
+        environment: {
+            // add env variables
+          myTestVar: "test",
+        },
+      },
+    });
+
+    // Health check
+    backendService.targetGroup.configureHealthCheck({ path: "/health" });
+
+    // Load balancer url
+    new cdk.CfnOutput(this, "loadBalancerUrl", {
+      value: backendService.loadBalancer.loadBalancerDnsName,
+      exportName: "loadBalancerUrl",
+    });
+  }
+}
---
+++ test/nestjs-template.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as NestjsTemplate from '../lib/nestjs-template-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/nestjs-template-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new NestjsTemplate.NestjsTemplateStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
